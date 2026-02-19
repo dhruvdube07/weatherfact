6fa11f4a63d07d9d40cbdc6a26789ebcaf80f73b
Dhruv Dubey <dubeydhruv2007@gmail.com>
2026-02-19 19:09:03 +0530

    Add WeatherData class with weather attributes

--- WeatherData.java
+++ WeatherData.java
@@ -1,1 +1,18 @@
+package com.weatherfact;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WeatherData {
+    private String location;
+    private double temperature;
+    private double feelsLike;
+    private int humidity;
+    private double windSpeed;
+    private int aqi;
+    private String status;
+}
